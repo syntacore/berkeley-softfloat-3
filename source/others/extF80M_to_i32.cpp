7781ba85e6c98bea6d722a7f142b516b575dc7b3
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2018-01-15 15:24:06 +0300

    chore(source/others/extF80M_to_i32.cpp)

--- source/others/extF80M_to_i32.cpp
+++ source/others/extF80M_to_i32.cpp
@@ -45,7 +45,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 int32_t
 extF80M_to_i32(
-    const extFloat80_t *aPtr, uint8_t roundingMode, bool exact)
+    const extFloat80_t* aPtr, uint8_t roundingMode, bool exact)
 {
 
     return extF80_to_i32(*aPtr, roundingMode, exact);
@@ -55,45 +55,39 @@ extF80M_to_i32(
 #else
 
 int32_t
-extF80M_to_i32(
-    const extFloat80_t *aPtr, uint8_t roundingMode, bool exact)
+extF80M_to_i32(extFloat80_t const* const aPtr,
+               uint8_t const roundingMode,
+               bool const exact)
 {
-    extFloat80M const *aSPtr;
-    uint16_t uiA64;
-    bool sign;
-    int32_t exp;
-    uint64_t sig;
-    int32_t shiftDist;
+    uint16_t const uiA64 = aPtr->signExp;
+    bool const sign = signExtF80UI64(uiA64);
+    int32_t const exp = expExtF80UI64(uiA64);
+    uint64_t sig = aPtr->signif;
+    int32_t const shiftDist = 0x4032 - exp;
 
-    aSPtr = aPtr;
-    uiA64 = aSPtr->signExp;
-    sign = signExtF80UI64(uiA64);
-    exp = expExtF80UI64(uiA64);
-    sig = aSPtr->signif;
-
-    shiftDist = 0x4032 - exp;
     if (shiftDist <= 0) {
         if (sig >> 32) {
-            goto invalid;
+            softfloat_raiseFlags(softfloat_flag_invalid);
+            return
+                0x7FFF == exp && 0 != (sig & UINT64_C(0x7FFFFFFFFFFFFFFF)) ? i32_fromNaN :
+                sign ? i32_fromNegOverflow :
+                i32_fromPosOverflow;
         }
+
         if (-32 < shiftDist) {
             sig <<= -shiftDist;
-        } else {
-            if ((uint32_t)sig) {
-                goto invalid;
-            }
+        } else if (0 != sig) {
+            softfloat_raiseFlags(softfloat_flag_invalid);
+            return
+                0x7FFF == exp && 0 != (sig & UINT64_C(0x7FFFFFFFFFFFFFFF)) ? i32_fromNaN :
+                sign ? i32_fromNegOverflow :
+                i32_fromPosOverflow;
         }
     } else {
-        sig = softfloat_shiftRightJam64(sig, shiftDist);
+        sig = softfloat_shiftRightJam64(sig, static_cast<uint32_t>(shiftDist));
     }
+
     return softfloat_roundPackToI32(sign, sig, roundingMode, exact);
-
-invalid:
-    softfloat_raiseFlags(softfloat_flag_invalid);
-    return
-        (exp == 0x7FFF) && (sig & UINT64_C(0x7FFFFFFFFFFFFFFF)) ? i32_fromNaN
-        : sign ? i32_fromNegOverflow : i32_fromPosOverflow;
-
 }
 
 #endif
