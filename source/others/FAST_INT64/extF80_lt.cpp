d7146d89d3c3eeef3146eb2ecb5fc674287b5202
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2018-01-19 02:07:38 +0300

    FIX(source/others/FAST_INT64/extF80_lt.cpp) proper operations priority

--- source/others/FAST_INT64/extF80_lt.cpp
+++ source/others/FAST_INT64/extF80_lt.cpp
@@ -58,6 +58,6 @@ extF80_lt(extFloat80_t a,
     bool const signB = signExtF80UI64(uiB64);
     return
         signA != signB ? signA && (((uiA64 | uiB64) & 0x7FFF) | uiA0 | uiB0) : 
-        uiA64 != uiB64 || uiA0 != uiB0 && (signA ^ softfloat_lt128(uiA64, uiA0, uiB64, uiB0));
+        (uiA64 != uiB64 || uiA0 != uiB0) && (signA ^ softfloat_lt128(uiA64, uiA0, uiB64, uiB0));
 }
 
