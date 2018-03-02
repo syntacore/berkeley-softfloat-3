1842efbed04ab491b11a5f7cc12b821d1586c5a2
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2018-03-02 15:37:27 +0300

    refactoring(source/others/s_roundPackToUI32.cpp): remove predefined condition

--- source/others/s_roundPackToUI32.cpp
+++ source/others/s_roundPackToUI32.cpp
@@ -45,36 +45,32 @@ softfloat_roundPackToUI32(bool sign,
                           uint8_t roundingMode,
                           bool exact)
 {
-    using namespace softfloat::internals;
     uint16_t const roundIncrement =
         softfloat_round_near_even == roundingMode || softfloat_round_near_maxMag == roundingMode ? 0x800u :
         (sign ? softfloat_round_min : softfloat_round_max) == roundingMode ? 0xFFFu :
-        0;
+        0u;
     uint16_t const roundBits = sig & 0xFFF;
     sig += roundIncrement;
 
-    if (0 != (sig & UINT64_C(0xFFFFF00000000000))) {
+    if (0 != (UINT64_C(0xFFFFF00000000000) & sig)) {
         softfloat_raiseFlags(softfloat_flag_invalid);
         return sign ? ui32_fromNegOverflow : ui32_fromPosOverflow;
+    } else {
+        bool const low_bit =
+            0 == (roundBits ^ UINT16_C(0x800)) &&
+            softfloat_round_near_even == roundingMode;
+
+        uint32_t const z = (sig >> 12) & ~static_cast<uint32_t>(!!low_bit);
+
+        if (sign && 0 != z) {
+            softfloat_raiseFlags(softfloat_flag_invalid);
+            return ui32_fromNegOverflow;
+        } else if (exact && 0 != roundBits) {
+            softfloat_raiseFlags(softfloat_flag_inexact);
+        }
+
+        return z;
     }
-
-    bool const low_bit =
-        0 == (roundBits ^ 0x800u) &&
-        softfloat_round_near_even == roundingMode;
-    uint32_t const z =
-        (sig >> 12) &
-        ~static_cast<uint32_t>(!!low_bit);
-
-    if (sign && 0 != z) {
-        softfloat_raiseFlags(softfloat_flag_invalid);
-        return sign ? ui32_fromNegOverflow : ui32_fromPosOverflow;
-    }
-
-    if (exact && 0 != roundBits) {
-        softfloat_raiseFlags(softfloat_flag_inexact);
-    }
-
-    return z;
 }
 
 }  // namespace internals
