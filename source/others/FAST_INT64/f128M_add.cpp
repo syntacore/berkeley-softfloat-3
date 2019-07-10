b0e96b075c8937f4701cd1bc26b62e6aca6b89c2
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2019-07-10 14:51:53 +0300

    chore source/others/FAST_INT64/f128M_add.cpp: apply single assignment const style

--- source/others/FAST_INT64/f128M_add.cpp
+++ source/others/FAST_INT64/f128M_add.cpp
@@ -37,28 +37,21 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "target.hpp"
 
 void
-f128M_add(const float128_t* aPtr,
-          const float128_t* bPtr,
-          float128_t* zPtr)
+f128M_add(float128_t const *const aPtr,
+          float128_t const *const bPtr,
+          float128_t* const zPtr)
 {
     using namespace softfloat::internals;
-    const uint64_t* aWPtr, *bWPtr;
-    uint64_t uiA64, uiA0;
-    bool signA;
-    uint64_t uiB64, uiB0;
-    bool signB;
-    aWPtr = (const uint64_t*)aPtr;
-    bWPtr = (const uint64_t*)bPtr;
-    uiA64 = aWPtr[indexWord(2, 1)];
-    uiA0 = aWPtr[indexWord(2, 0)];
-    signA = signF128UI64(uiA64);
-    uiB64 = bWPtr[indexWord(2, 1)];
-    uiB0 = bWPtr[indexWord(2, 0)];
-    signB = signF128UI64(uiB64);
 
-    if (signA == signB) {
-        *zPtr = softfloat_addMagsF128(uiA64, uiA0, uiB64, uiB0, signA);
-    } else {
-        *zPtr = softfloat_subMagsF128(uiA64, uiA0, uiB64, uiB0, signA);
-    }
+    uint64_t const* const aWPtr = reinterpret_cast<uint64_t const*>(aPtr);
+    uint64_t const* const bWPtr = reinterpret_cast<uint64_t const*>(bPtr);
+    uint64_t const uiA64 = aWPtr[indexWord(2, 1)];
+    uint64_t const uiA0 = aWPtr[indexWord(2, 0)];
+    bool const signA = signF128UI64(uiA64);
+
+    uint64_t const uiB64 = bWPtr[indexWord(2, 1)];
+    uint64_t const uiB0 = bWPtr[indexWord(2, 0)];
+    bool const signB = signF128UI64(uiB64);
+
+    *zPtr = (signA == signB ? softfloat_addMagsF128 : softfloat_subMagsF128)(uiA64, uiA0, uiB64, uiB0, signA);
 }
