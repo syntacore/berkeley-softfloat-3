255b0028eb1367504d3d8a1a390467318ac2a9c3
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2019-07-13 22:32:29 +0300

    chore(source/primitives/FAST_INT64/s_mul128To256M.cpp)

--- source/primitives/FAST_INT64/s_mul128To256M.cpp
+++ source/primitives/FAST_INT64/s_mul128To256M.cpp
@@ -44,23 +44,20 @@ namespace softfloat {
 namespace internals {
 
 void
-softfloat_mul128To256M(uint64_t a64,
-                       uint64_t a0,
-                       uint64_t b64,
-                       uint64_t b0,
+softfloat_mul128To256M(uint64_t const a64,
+                       uint64_t const a0,
+                       uint64_t const b64,
+                       uint64_t const b0,
                        uint64_t* zPtr)
 {
-    uint128 p0, p64, p128;
-    uint64_t z64, z128, z192;
-
-    p0 = softfloat_mul64To128(a0, b0);
+    uint128 const p0 = softfloat_mul64To128(a0, b0);
     zPtr[indexWord(4, 0)] = p0.v0;
-    p64 = softfloat_mul64To128(a64, b0);
-    z64 = p64.v0 + p0.v64;
-    z128 = p64.v64 + (z64 < p64.v0);
-    p128 = softfloat_mul64To128(a64, b64);
+    uint128 p64 = softfloat_mul64To128(a64, b0);
+    uint64_t z64 = p64.v0 + p0.v64;
+    uint64_t z128 = p64.v64 + (z64 < p64.v0);
+    uint128 const p128 = softfloat_mul64To128(a64, b64);
     z128 += p128.v0;
-    z192 = p128.v64 + (z128 < p128.v0);
+    uint64_t const z192 = p128.v64 + (z128 < p128.v0);
     p64 = softfloat_mul64To128(a0, b64);
     z64 += p64.v0;
     zPtr[indexWord(4, 1)] = z64;
